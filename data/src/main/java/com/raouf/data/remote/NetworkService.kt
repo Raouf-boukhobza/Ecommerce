636c9d1d3ac32d5a5f02043770dca737fdf7dbcd
raouf <raoufboukhobza3@gmail.com>
2024-09-27 21:14:45 +0100

    fix the network service error

--- data/src/main/java/com/raouf/data/remote/NetworkService.kt
+++ data/src/main/java/com/raouf/data/remote/NetworkService.kt
@@ -3,20 +3,22 @@ package com.raouf.data.remote
 import retrofit2.http.GET
 import retrofit2.http.Path
 
-interface NetworkService  {
+interface NetworkService {
 
     @GET("products/category/{categoryName}")
-    suspend fun getProducts (
-        @Path("categoryName") categoryName : String
-    ) : ProductDto
+    suspend fun getProducts(
+        @Path("categoryName") categoryName: String
+    ): ProductDto
 
     @GET("products/{id}")
     suspend fun getProductById(
-        @Path("id") id : Int
-    ) : ProductDtoItem
+        @Path("id") id: Int
+    ): ProductDtoItem
 
     @GET("products/categories")
-    suspend fun getCategories () : ArrayList<String>
+    suspend fun getCategories(): ArrayList<String>
+
+}
 
 
 
