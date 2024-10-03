b23dff4d24b75fe706b663315d0d568af1d9f930
raouf <raoufboukhobza3@gmail.com>
2024-10-03 20:26:35 +0100

    add the review view

--- presentation/src/main/java/com/raouf/ecommerceapp/ui/detail/DetailsScreen.kt
+++ presentation/src/main/java/com/raouf/ecommerceapp/ui/detail/DetailsScreen.kt
@@ -8,6 +8,9 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -21,14 +24,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImagePainter
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
 import coil.size.Size
 import com.raouf.domain.model.Product
+import com.raouf.ecommerceapp.ui.theme.orange
 
 @Composable
 fun DetailScreen(
@@ -48,6 +54,7 @@ fun DetailScreen(
                 product = product
             )
             ReviewView(product = product)
+
         }
     }
 }
@@ -60,7 +67,6 @@ fun ImagesView(
     product: Product
 ) {
     Column(
-        modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         val imageState = rememberAsyncImagePainter(
@@ -127,18 +133,26 @@ fun ImagesView(
 fun ReviewView(
     product: Product
 ) {
-    Row {
-       /* Icon(
-            imageVector = Icons.Rounded.Star,
-            contentDescription = null,
-            tint = Color.Gray,
-            modifier = Modifier.size(32.dp)
-        )*/
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(100.dp)
+            .padding(horizontal = 8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+         Icon(
+             imageVector = Icons.Rounded.Star,
+             contentDescription = null,
+             tint = orange,
+             modifier = Modifier.size(26.dp),
+         )
         Text(
             text = product.rating.toString(),
             fontStyle = FontStyle.Italic,
             fontWeight = FontWeight.Medium,
-            color = Color.Gray
+            fontSize = 20.sp,
+            color = orange
         )
 
     }
